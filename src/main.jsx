c4053ac2091e9eac837794b5d9c0109b9a004755
26th-Official <you@example.com>
2024-02-13 15:41:31 +0530

    final template

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Toaster } from "@/components/ui/toaster"
 
 import App from './App.jsx'
 import './index.css'
@@ -8,6 +7,5 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Toaster />
   </React.StrictMode>,
 )
